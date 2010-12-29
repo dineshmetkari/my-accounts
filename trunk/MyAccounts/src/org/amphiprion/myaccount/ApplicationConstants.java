a53c2f92f5870466635cd5b0c80ad5b233430ab9
amphiprions <amphiprions@b2eb863e-d0bb-63bf-bba6-652af697ead3>
2010-12-29 14:07:21 +0000

    Add period support for operation list (todo custom range)

--- trunk/MyAccounts/src/org/amphiprion/myaccount/ApplicationConstants.java
+++ trunk/MyAccounts/src/org/amphiprion/myaccount/ApplicationConstants.java
@@ -28,6 +28,8 @@ package org.amphiprion.myaccount;
 public interface ApplicationConstants {
 	public final static String PACKAGE = "org.amphiprion.myaccount";
 	public final static String NAME = "MyAccounts";
+	public static final String IMPORT_DRIRECTORY = "import";
+
 	public static final int MENU_ID_ADD_ACCOUNT = 1;
 	public static final int MENU_ID_ADD_CATEGORY = 2;
 	public static final int MENU_ID_EDIT_CATEGORY = 3;
@@ -35,6 +37,7 @@ public interface ApplicationConstants {
 	public static final int MENU_ID_IMPORT_OPERATION = 5;
 	public static final int MENU_ID_EDIT_ACCOUNT = 6;
 	public static final int MENU_ID_DELETE_ACCOUNT = 7;
+	public static final int MENU_ID_CHANGE_PERIOD_OPERATION = 8;
 
 	public static final int ACTIVITY_RETURN_CREATE_ACCOUNT = 1;
 	public static final int ACTIVITY_RETURN_EDIT_ACCOUNT = 2;
@@ -43,6 +46,5 @@ public interface ApplicationConstants {
 	public static final int ACTIVITY_RETURN_IMPORT_OPERATION = 5;
 	public static final int ACTIVITY_RETURN_CHOOSE_FILE = 6;
 	public static final int ACTIVITY_RETURN_VIEW_OPERATION_LIST = 7;
-	public static final String IMPORT_DRIRECTORY = "import";
 
 }
